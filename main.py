fd8f60fda191476731eb59a72b7d762bddddc7a8
daje0601 <englishmt4118@gmail.com>
2020-12-11 23:13:28 +0900

    success local webpage open
    정말 간단한건데요.. 혼자 독학으로 하려니 이런대서 시간을 다 까먹는다요!

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from flask import Flask, render_template, escape, request
+import socket
+
+socket.gethostbyname("")
 
 app = Flask(__name__)
 
- 
 @app.route("/")
 def home():
     return "Hello! this is the main page"
     # return render_template("home.html") 
 
 # @는 데코레이터로 바로 아래에 있는 함수를 찾는데 그 함수를 데코레이트 해주는 역할을 해 
-
 @app.route("/report")
 def report():
     print(request.args.get("word"))
     return "This is a text page"
 
-if __name__ == "__main__":
-    app.run()
-
+app.run(host="127.0.0.1")
