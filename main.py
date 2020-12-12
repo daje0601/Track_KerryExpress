98f66015c98c5ada74acceaff807f9a8f0fab929
daje0601 <englishmt4118@gmail.com>
2020-12-12 14:20:16 +0900

    add templates(home,report)

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-from flask import Flask, render_template, escape, request
-import socket
+from flask import Flask, render_template, request
+#request는 우리가 입력한 자료를 받아오기 위해서 import하는거야 
 
-socket.gethostbyname("")
-
-app = Flask(__name__)
+app = Flask("SuperScrapeer")
 
 @app.route("/")
 def home():
-    return "Hello! this is the main page"
-    # return render_template("home.html") 
-
+    return render_template("home.html")
 # @는 데코레이터로 바로 아래에 있는 함수를 찾는데 그 함수를 데코레이트 해주는 역할을 해 
 @app.route("/report")
 def report():
-    print(request.args.get("word"))
-    return "This is a text page"
+    word = request.args.get("word")
+    return render_template("report.html", searchingBy=word)
 
 app.run(host="127.0.0.1")
+
